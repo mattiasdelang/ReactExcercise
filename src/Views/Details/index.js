2bd0f52b1af23d329f9e71c4a77d8117d38b4dfa
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-09-30 21:04:12 +0200

    Step3

--- src/Views/Details/index.js
+++ src/Views/Details/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import DetailsCard from './Components/DetailsCard';
 
 function Details(){
+    const {id} = useParams()
+
+    const [pokemon, setPokemon] = useState(null)
+
+    useEffect(()=>{
+        Axios.get(`http://localhost:1337/pokemon/${id}`)
+            .then(res=>setPokemon(res.data))
+    },[id])
+    
     return(
-        <div>Details</div>
+        <div>
+            {pokemon&&<DetailsCard {...pokemon}/>}
+        </div>
     )
 }
 
