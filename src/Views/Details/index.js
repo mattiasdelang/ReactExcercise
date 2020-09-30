00d2b3b91699139c953283f1033bd554ddd5514e
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-09-30 21:14:39 +0200

    step6

--- src/Views/Details/index.js
+++ src/Views/Details/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGet } from '../../Hooks/HttpHooks';
+import DetailsButton from './Components/DetailsButton';
 import DetailsCard from './Components/DetailsCard';
 
 function Details() {
@@ -8,10 +9,19 @@ function Details() {
 
     const { data, loading, error } = useGet(`http://localhost:1337/pokemon/${id}`)
 
-    if (loading) return <span>loading...</span>
-    if (data) return <DetailsCard {...data} />
-    if (error) return <span>{error}</span>
-    return null
+    let content = null
+    if (loading) content = <span>loading...</span>
+    if (data) content = <DetailsCard {...data} />
+    if (error) content = <span>{error}</span>
+    return (
+        <div className='detailsContainer'>
+            <div className='detailsNavigation'>
+                {id !== '1' && <DetailsButton text='back' id={parseInt(id) - 1} />}
+                {id !== '151' && <DetailsButton text='next' id={parseInt(id) + 1} />}
+            </div>
+            {content}
+        </div>
+    )
 }
 
 export default Details
