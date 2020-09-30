561428e243f6d50442ca5fadf2757c96272eb6a6
Steven Van de Poel <stevenvandepoel@gmail.com>
2020-09-30 21:05:34 +0200

    step4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { Header } from './Components';
 import { Details, Main } from './Views';
 
 function App() {
   return (
     <div className='appContainer'>
       <Router>
+        <Header/>
         <Switch>
           <Route exact path='/' component={Main} />
           <Route path='/:id' component={Details} />
